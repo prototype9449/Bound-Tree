8db50ab745193de81e5e62ca8dbdd942f2155de1
prototype9449 <prototype9449@yandex.ru>
2015-09-30 18:47:43 +0300

    [master] refactoring

--- BoundTree/BoundTree/Helpers/TreeFiller.cs
+++ BoundTree/BoundTree/Helpers/TreeFiller.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using BoundTree.Nodes;
 
 namespace BoundTree.Helpers
 {
@@ -22,15 +24,49 @@ namespace BoundTree.Helpers
             while (queue.Count != 0)
             {
                 var currentId = queue.Peek().Id;
+                var currentNode = tree.GetById(currentId);
                 if (dictionary.ContainsKey(currentId))
                 {
-                    tree.GetById(currentId).NodeInfo = dictionary[currentId].NodeInfo;
+                    currentNode.NodeInfo = dictionary[currentId].NodeInfo;
+                }
+                else
+                {
+                    currentNode.NodeInfo = new EmptyNodeInfo();
                 }
 
                 foreach (var node in queue.Dequeue().Nodes)
                 {
                     queue.Enqueue(node);
                 }
+            }
+        }
+
+        private void RestoreRestNodes(Tree tree, Tree minorTree, Dictionary<int, Node> dictionary)
+        {
+            var stack = new Stack<Node>();
+            stack.Push(tree.Root);
+            var markedNodes = new HashSet<Node>();
+
+            while (stack.Count != 0)
+            {
+                var currentNode = stack.Peek();
+                if (currentNode.Nodes.Count == 0)
+                {
+                    markedNodes.Add(currentNode);
+                    stack.Pop();
+                }
+                else if(currentNode.Nodes.All(markedNodes.Contains))
+                {
+                    currentNode.NodeInfo = TryGetCommonParent(currentNode.Nodes);
+                }
+                else
+                {
+                    foreach (var node in currentNode.Nodes)
+                    {
+                        stack.Push(node);
+                    }
+                }
+
             }
         }
     }
