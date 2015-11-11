73421cf0fe1abda27f990fdfc396523da3de0cc2
prototype9449 <prototype9449@yandex.ru>
2015-11-11 13:01:29 +0300

    [master] methods for grouping were added

--- BoundTree/BoundTree/Helpers/TreeReconstruction/PostReconstruction.cs
+++ BoundTree/BoundTree/Helpers/TreeReconstruction/PostReconstruction.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using BoundTree.Logic;
-using BoundTree.Logic.Nodes;
 
 namespace BoundTree.Helpers.TreeReconstruction
 {
@@ -17,17 +16,41 @@ namespace BoundTree.Helpers.TreeReconstruction
 
         public void Reconstruct(DoubleNode<T> doubleNode)
         {
-            var stack = new Stack<DoubleNode<T>>(new[] {doubleNode});
+            var stack = new Stack<DoubleNode<T>>(new[] { doubleNode });
             var passedNodes = new HashSet<DoubleNode<T>>();
+            var initialChildIds = doubleNode.Nodes.Select(node => node.MinorLeaf.Id).ToList();
 
             while (stack.Any())
             {
                 var current = stack.Pop();
 
-                if(!current.Nodes.Any() || passedNodes.Contains(current)) continue;
+                if (!current.Nodes.Any()) continue;
 
-                var descendants = current.Nodes.Select(node => GetRepairedNode(current, node));
+                if (passedNodes.Contains(current))
+                {
+                    current.Nodes.ForEach(node => passedNodes.Add(node));
+                    continue;
+                }
 
+                var descendants = current.Nodes.Select(node => GetRepairedNode(current, node)).ToList();
+                current.Nodes = descendants.Select(pair => pair.Value).ToList();
+                for (int i = 0; i < current.Nodes.Count; i++)
+                {
+                    var currentDoubleNode = descendants[i].Value;
+
+                    while (currentDoubleNode.Nodes.Count() == 1 && currentDoubleNode.MinorLeaf.Id != initialChildIds[i])
+                    {
+                        passedNodes.Add(currentDoubleNode);
+                        currentDoubleNode = currentDoubleNode.Nodes.First();
+                    }
+                }
+
+                if (descendants.Exists(node => node.Key == true))
+                {
+                    GroupDoubleNodes(current, initialChildIds);
+                    stack.Clear();
+                    stack.Push(doubleNode);
+                }
             }
         }
 
@@ -49,12 +72,46 @@ namespace BoundTree.Helpers.TreeReconstruction
             {
                 doubleAscendant = new DoubleNode<T>(new Node<T>(), singleAscendant.Node)
                 {
-                    Nodes = new List<DoubleNode<T>>(new[] {doubleAscendant})
+                    Nodes = new List<DoubleNode<T>>(new[] { doubleAscendant })
                 };
                 isDone = true;
             }
 
             return new KeyValuePair<bool, DoubleNode<T>>(isDone, doubleAscendant);
+        }
+
+
+        private void GroupDoubleNodes(DoubleNode<T> doubleNode, List<T> initialChildIds)
+        {
+            var queue = new Queue<DoubleNode<T>>(new[] { doubleNode });
+
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+                if(initialChildIds.Contains(current.MinorLeaf.Id)) continue;
+
+                var groupedNodes = doubleNode.Nodes
+                    .Where(node => !node.MainLeaf.IsEmpty())
+                    .GroupBy(node => node.MinorLeaf.Id)
+                    .Where(group => group.Count() > 1);
+
+                var repairedNodes = groupedNodes.Select(GetRepairedNode).ToList();
+
+                doubleNode.Nodes.RemoveAll(repairedNode 
+                    => repairedNodes.Exists(node => node.MinorLeaf.Id == repairedNode.MinorLeaf.Id));
+
+                repairedNodes.ForEach(doubleNode.Nodes.Add);
+            }
+        }
+
+        private DoubleNode<T> GetRepairedNode(IGrouping<T, DoubleNode<T>> group)
+        {
+            var doubleNode = new DoubleNode<T>(new Node<T>(), group.First().MinorLeaf)
+            {
+                Nodes = group.Select(node => node.Nodes.First()).ToList()
+            };
+
+            return doubleNode;
         }
     }
 }
