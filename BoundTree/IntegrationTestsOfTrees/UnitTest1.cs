a3c8f784e17197798878c87f1197172d03c40635
prototype9449 <prototype9449@yandex.ru>
2015-11-24 18:50:24 +0300

    [master] first test was added

---
+++ BoundTree/IntegrationTestsOfTrees/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Build.TestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTestsOfTrees
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string _pathToTestFolder;
+        private ValidationController _validator = new ValidationController();
+        public UnitTest1()
+        {
+            _pathToTestFolder = Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName,"Tests");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var path = Path.Combine(_pathToTestFolder, "test1.txt");
+            Assert.IsTrue(_validator.IsValid(path));
+        }
+    }
+}
